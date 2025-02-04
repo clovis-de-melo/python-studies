671372c598459db9f72ea800ed9dfc6742b7d85b
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-02-04 19:58:52 -0300

    feat: readme, operators

---
+++ operators.py
@@ -0,0 +1,31 @@
+print("Welcome to Python!") # words and phrases need quote
+print(29) # number does not need quote
+
+# Variables 
+
+revenue = 1000
+cost = 300 
+
+print("Profit:", revenue - cost)
+
+# Python Arithmetic Operators
+
+# + addition x + y 
+# - subtraction x - y 
+# * multiplication x * y 
+# / division  x / y 
+# % Modulus x % y
+# ** exponentiation x ** y x = 2, y= 5 same as 2*2*2*2*2
+# // Floor division x // y  the floor division // rounds the result down to the nearest whole number
+
+print(100 + 20)
+
+# Python Assignment Operators
+
+# = 
+# example: x = 5
+# same as: x = 5 
+
+# +=
+# example: x += 3
+# same as:x = x + 3
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Python studies
+
+## Developer commands
+
