e42ff5a3db8cb675da6e885a905e49834ffecbed
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-02-04 20:44:47 -0300

    feat: add more operators

--- operators.py
+++ operators.py
@@ -1,14 +1,5 @@
-print("Welcome to Python!") # words and phrases need quote
-print(29) # number does not need quote
-
-# Variables 
-
-revenue = 1000
-cost = 300 
-
-print("Profit:", revenue - cost)
-
 # Python Arithmetic Operators
+print("Python Arithmetic Operators")
 
 # + addition x + y 
 # - subtraction x - y 
@@ -19,8 +10,15 @@ print("Profit:", revenue - cost)
 # // Floor division x // y  the floor division // rounds the result down to the nearest whole number
 
 print(100 + 20)
+print(100 - 20)
+print(100 * 20)
+print(100 / 20)
+print(100 % 20)
+print(100 ** 20)
+print(100 // 20)
 
 # Python Assignment Operators
+print("Python Assignment Operators")
 
 # = 
 # example: x = 5
@@ -28,4 +26,82 @@ print(100 + 20)
 
 # +=
 # example: x += 3
-# same as:x = x + 3
+# same as: x = x + 3
+
+
+# Python Comparison Operators
+print("Python Comparison Operators")
+
+# == 
+# Name: Equal 
+# Example: x == y
+# x= 5, y = 3 
+# print(x == y)
+# False
+# returns False because 5 is not equal to 3
+
+x = 5
+y = 3
+print("5 == 3", x == y)
+
+x = 5
+y = 5
+print("5 == 5", x == y)
+
+# != 
+# Name: Not equal
+# Example: x != y
+# x = 5, y = 3
+# print(x != y)
+# True
+# returns True because 5 is not equal to 3
+
+x = 5
+y = 3
+print("5 != 3", x != y)
+
+# >
+# Name: Greater than
+# Example: x > y
+# x = 5, y = 3
+# print(x > y)
+# True
+# returns True because 5 is greater than 3
+
+x = 5
+y = 3
+print("5 > 3", x > y)
+
+# < 
+# Name: Less than
+# Example: x < y
+# x = 5, y = 3
+# False
+# returns False because 5 is not less than 3
+
+# >= 
+# Name: Greater than or equal to
+# Example: x >= y
+# x = 5, y = 3
+# True
+# returns True because five is greater, or equal, to 3
+
+# <= 
+# Name: Less than or equal to
+# Example: x <= y
+# x = 5, y = 3
+# False
+# returns False because 5 is neither less than or equal to 3
+
+
+
+
+
+# Python Logical Operators
+print("Python Logical Operators")
+
+# and
+
+# or 
+
+# not
--- readme.md
+++ readme.md
@@ -2,3 +2,6 @@
 
 ## Developer commands
 
+## References
+
+[W3 Schools - Python](https://www.w3schools.com/python/python_operators.asp)
