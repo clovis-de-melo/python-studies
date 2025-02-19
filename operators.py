637aa4c30667ad76ac818b630d54e27651d47c8d
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-02-19 20:15:20 -0300

    feat: update operators.py

--- operators.py
+++ operators.py
@@ -1,4 +1,26 @@
+# Python Operators
+
+# Operators are used to perform operations on variables and values.
+
+# In the example below, we use the "+" operator to add together two values: 
+
+# Example: print(10 + 5)
+
+# Python divides the operators in the following groups: 
+
+# Arithmetic operators
+# Assignment operators
+# Comparison operators
+# Logical operators
+# Identity operators
+# Membership operators
+# Bitwise operators
+
+
 # Python Arithmetic Operators
+
+# Arithmetic operators are used with numeric values to perform common mathematical operations:
+
 print("Python Arithmetic Operators")
 
 # + addition x + y 
@@ -9,111 +31,145 @@ print("Python Arithmetic Operators")
 # ** exponentiation x ** y x = 2, y= 5 same as 2*2*2*2*2
 # // Floor division x // y  the floor division // rounds the result down to the nearest whole number
 
-print(100 + 20)
-print(100 - 20)
-print(100 * 20)
-print(100 / 20)
-print(100 % 20)
-print(100 ** 20)
-print(11 // 2) # 5 the floor division // rounds the result down to the nearest whole number
+print(100 + 20) # 120
+print(100 - 20) # 80
+print(100 * 20) # 2000
+print(100 / 20) # 5
+print(100 % 20) # 0
+print(10 ** 20) # 100000000000000000000
+print(11 // 2) # 5 
 
 # Python Assignment Operators
+
+# Assignment operators are used to assign values to variables:
+
 print("Python Assignment Operators")
 
-# = 
-# Example: x = 5
-# same as: x = 5 
+# = , x = 5, same as x =5 
+# += , x += 3, same as x = x + 3
+# -= , x -= 3, same as x = x + 3
+# -= , x -= 3, same as x = x -3 
+# *= , x *= 3, same as x = x * 3
+# /= , x /= 3, same as x = x / 3
+# %= , x %= 3, same as x = x % 3
+# //= , x //= 3, same as x = // 3
+# **= , x **= 3, same as x = x ** 3   
+# &= , x &=3 , same as x = x & 3
+# |= , x |= 3 , same as x = x | 3
+# ^= , x ^= 3, same as x = x ^ 3
+# >>= , x >>= 3, same as x = x >> 3
+# <<= , x <<= 3, same as x = x << 3
+# := , print(x:=3), same as x = 3(printx)
+
+
+# Practice
+
+# =  
 
 x = 5
-print(x)
+print(x) # 5
 
 # +=
-# Example: x += 3
-# same as: x = x + 3
 
 x = 5 
 x += 3
-print(x)
+print(x) # 8
 
 # -=
-# Example: x -= 3
-# same a: x = x * 3
 
 x = 5 
 x -= 3
 print(x)
 
+# *=
+
+x = 5
+x *= 3
+print(x)
+
 
 # Python Comparison Operators
+
 print("Python Comparison Operators")
 
+# Comparison operators are used to compare two values:
+
+# == , Equal, x == y
+# != , Not equal, x != y
+# > , Greater than, x > y 
+# < , Less than , x < y
+#  >= , Greater than or equal to, x >= y
+# <= , Less than or equal to, x <= y
+
+# Practice
+
 # == 
-# Name: Equal 
-# Example: x == y
-# x= 5, y = 3 
-# print(x == y)
-# False
-# returns False because 5 is not equal to 3
 
 x = 5
 y = 3
-print("5 == 3", x == y)
+print(x == y) # false
 
 x = 5
 y = 5
-print("5 == 5", x == y)
+print(x == y) # true
 
 # != 
-# Name: Not equal
-# Example: x != y
-# x = 5, y = 3
-# print(x != y)
-# True
-# returns True because 5 is not equal to 3
 
 x = 5
 y = 3
-print("5 != 3", x != y)
+print(x != y) # true
+
+x = 5
+y = 5
+print(x != y) # false
 
 # >
-# Name: Greater than
-# Example: x > y
-# x = 5, y = 3
-# print(x > y)
-# True
-# returns True because 5 is greater than 3
 
 x = 5
 y = 3
-print("5 > 3", x > y)
+print(x > y) # true
+
+x = 5
+y = 5
+print(x > y) # false
 
 # < 
-# Name: Less than
-# Example: x < y
-# x = 5, y = 3
-# False
-# returns False because 5 is not less than 3
+
+x = 5
+y = 3
+print(x < y) # false
+
+x = 8
+y = 7
+print(x > y) # true
+
 
 # >= 
-# Name: Greater than or equal to
-# Example: x >= y
-# x = 5, y = 3
-# True
-# returns True because five is greater, or equal, to 3
+
+x = 5
+y = 5
+print(x >= y) # true
+
+x = 2 
+y = 6
+print(x >= y) # false
+
 
 # <= 
-# Name: Less than or equal to
-# Example: x <= y
-# x = 5, y = 3
-# False
-# returns False because 5 is neither less than or equal to 3
 
+x = 5
+y = 5
+print(x <= y) # true
 
-
-
+x = 6
+y = 11
+print(x >= y) # false
 
 # Python Logical Operators
+
 print("Python Logical Operators")
+
+# Logical operators are used to combine conditional statements
 
 # and
 
