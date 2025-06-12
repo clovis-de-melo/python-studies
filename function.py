f63d1662fc0c5ca6acbc53799d7dbba1cd8e336f
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-06-11 23:05:15 -0300

    feat: creating a function

--- function.py
+++ function.py
@@ -1,1 +1,24 @@
 # Python Function 
+# A function is a block of code which only runs when it is called.
+# You can pass data, known as parameters, into a function.
+# A function can return data as a result
+
+# https://www.w3schools.com/python/python_functions.asp
+
+# Creating a Function
+# In Python a function is defined using the def keyword
+
+def my_function():
+    print("Hello from a function")
+
+# Calling a Function
+# To call a function, use the function name followed by parenthesis
+
+def my_function():
+    print("Hello from function")
+
+my_function()
+
+# Arguments 
+
+
