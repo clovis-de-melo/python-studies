792e65a1d0335a1c9c6b6b1c1bc5e315f8d65c63
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-06-15 22:13:19 -0300

    feat: if, elif conditional statement

---
+++ conditional-statement.py
@@ -0,0 +1,37 @@
+# In Python, the if statement is a conditional statement that executes a block of code only if a specified condition is true. 
+# It is a fundamental part of decision-making in programming.
+
+# https://www.w3schools.com/python/python_conditions.asp
+
+# Python supports the usual logical conditions from mathematics
+
+""" 
+Equals: a == b
+Not Equals: a != b
+Less than: a < b
+Less than or equal to: a <= b
+Greater than: a > b
+Greater than or equal to: a >= b
+"""
+
+# These conditions can be used in several ways, most commonly in "if statements" and loops.
+#An "if statement" is written by using the if keyword
+
+# Example
+
+# if statement 
+
+a = 33
+b = 200
+if b > a:
+    print("b is greater than a")
+
+# elif
+# The elif keyword is Python's way of saying "if the previous conditions were not true, then try this condition".
+
+c = 33
+d = 33
+if d > c:
+    print("d is greater than c")
+elif c == d:
+    print("c and d are equal")    
