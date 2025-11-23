22a6ed0556fd063d356072c15c5960f8bb945574
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-11-22 23:25:37 -0300

    feat: list exercise

--- 2-core-syntax-and-data-structures/lists.py
+++ 2-core-syntax-and-data-structures/lists.py
@@ -3,3 +3,35 @@
 """ A list in Python is a collection that can store multiple items (like numbers, words, or other objects) in a single variable.
 Lists are ordered, changeable (you can add, remove, or modify elements), and allow duplicate values. """
 
+# Exercicio 1: criando e acessando listas 
+
+# Crie uma lista com 5 frutas de sua preferência
+# Mostre a primeira fruta
+# Mostre a última fruta
+# Mostre todas as frutas
+# Desafio: Tente imprimir a terceira fruta da lista
+
+frutas = ["maçã", "banana", "laranja", "uva", "morango"]
+
+print(frutas[0])
+print(frutas[-1])
+print(frutas)
+print(frutas[3])
+
+# Exercício 2: Adicionando Itens
+
+# Lista de compras inicial
+# Adicione "leite" no final da lista
+# Adicione "pão" no início da lista
+# Mostre a lista completa
+# Desafio: Adicione mais 2 itens que você gostaria de comprar!
+
+compras = ["arroz", "feijão", "macarrão"]
+
+compras.append("leite")
+
+compras.insert(0, "pão")
+
+print(compras)
+
+
