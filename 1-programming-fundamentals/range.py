1da095c0b191804e6a41b68cb1de6a3a1e8b3914
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-11-29 21:51:43 -0300

    feat: add range.py file

---
+++ 1-programming-fundamentals/range.py
@@ -0,0 +1,24 @@
+# The range() function in Python generates a sequence of immutable numbers, commonly used for iterating a specific number of times in for loops. It returns a range object, which is an iterable but not a list itself, making it memory-efficient for large sequences.
+
+# range() é uma função em Python que gera uma sequência de números inteiros, usada principalmente para controlar iterações em for loops. A sequência é imutável e pode ser definida por até três argumentos: start (início), stop (fim) e step (pulo ou incremento
+
+for i in range(3):
+    print("Hello")
+
+""" 
+Hello
+Hello
+Hello 
+"""
+
+for i in range(5):
+    print(i)
+
+""" 
+0
+1
+2
+3
+4
+5 
+"""
