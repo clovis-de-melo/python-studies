2f2199539fd224d9f8d13294416cced99a8bd5ab
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-12-29 19:25:27 -0300

    feat: slicing review

--- 2-core-syntax-and-data-structures/slicing.py
+++ 2-core-syntax-and-data-structures/slicing.py
@@ -39,6 +39,7 @@ def lines():
     print("-" * 50)
     print("\n")
 
+
 # Exercicio 1 - String básica 
 print("\nString exercise")
 
@@ -100,7 +101,7 @@ print(cores[1:-1])
 | --------------------------- | -------- |
 | Pular o primeiro            | [1:]   |
 | Pular o último              | [:-1]  |
-| Pular primeiro **e** último | [1:-1] | 
+| Pular primeiro e último     | [1:-1] | 
 """
 
 lines()
@@ -121,14 +122,137 @@ print(inverted_list)
 lines()
 
 # Exercicio 6 - Slicing + Loop 
-print("\nSlicing + Loop")
+print("Slicing + Loop")
 
 numeros = [10, 20, 30, 40, 50, 60, 70, 80]
 
 # Use slicing para pegar apenas os números que estão nas posições pares (índices 0, 2, 4, 6).
 
+print(numeros[::2])
+
+# lista[inicio:fim:passo]
+
+""" início → vazio (começa no índice 0)
+
+fim → vazio (vai até o final)
+
+passo = 2 → pula de 2 em 2 índices """
 
 # Depois, use um loop para imprimir cada um desses números multiplicado por 2.
 
+numeros_posicao_par = numeros[::2]
+
+for numero in numeros_posicao_par:
+    print(numero * 2)
 
 
+lines()
+
+# Review 
+
+# sequence[start:end:step]
+
+print("Review")
+print("\n")
+
+# Exercise 1: Slicing básico (string)
+
+palavra = "Python"
+
+# Mostre as 4 primeiras letras 
+print(palavra[:4])
+
+lines()
+
+# Exercise 2: Slicing básico (lista)
+
+numeros = [10, 20, 30, 40, 50]
+
+# Mostre os 3 primeiros números
+print(numeros[:3])
+
+lines()
+
+# Exercise 3 — Últimos elementos
+
+frutas = ["maçã", "banana", "laranja", "uva", "pera"]
+
+# Mostre as 2 últimas frutas
+print(frutas[-2:])
+
+lines()
+
+# Exercise 4 - Exceto o último
+
+cores = ["vermelho", "azul", "verde", "amarelo"]
+
+# Mostre todas as cores exceto a última
+print(cores[0:3])
+
+lines()
+
+# Exercise 5 - Índices específicos 
+
+numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+letras = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+# Mostre do índice 2 ao índice 6
+print(len(numeros))
+print(numeros[2:6])
+
+# Mostre os números do 3 até o 7, incluindo o 7, usando slicing
+print(numeros[3:8])
+
+lines()
+
+# Exercise 6 -Step (pulando elementos)
+
+numeros = [10, 20, 30, 40, 50, 60, 70, 80]
+
+# mostre os numeros pulando de 2 em 2 
+print(numeros[::2])
+
+# mostre os numeros pulando de 1 em 1 
+print(numeros[::1])
+
+# mostre os numeros pulando de 3 em 3 
+print(numeros[::3])
+
+lines()
+
+# Exercise 7 - Step + inicio 
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8]
+
+# Mostre os números das posições pares
+print(numeros[::2])
+
+lines()
+
+# Exercise 8 - Inverter sequência 
+
+palavra = "Python"
+
+# Inverta a palavra usando slicing
+print(palavra[::-1])
+
+lines()
+
+# Exercise 9 - Parte do meio 
+
+texto = "Programacao"
+
+# Mostre apenas "grama"
+print(texto[3:8])
+
+lines()
+
+# Exercise 10 - Slicing + lógica 
+
+numeros = [10, 20, 30, 40, 50, 60, 70, 80]
+
+# Pegue os números do índice 1 ao 6 pulando de 2 em 2
+print(numeros[1:6:2])
+
+lines()
