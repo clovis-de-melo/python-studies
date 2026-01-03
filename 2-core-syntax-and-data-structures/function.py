cb1f995f4f4a2d5dea5a0ba39164121b0c0b52c2
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2026-01-02 21:05:12 -0300

    feat: exercise 1 to 7

--- 2-core-syntax-and-data-structures/function.py
+++ 2-core-syntax-and-data-structures/function.py
@@ -156,3 +156,318 @@ def soma(a, b):
 resultado = soma(2, 3)
 print(resultado)
 
+
+# return é a palavra que devolve um valor de dentro de uma função para quem chamou essa função.
+
+""" A função é uma máquina
+Você coloca dados (parâmetros)
+A máquina processa
+O return é o resultado que sai da máquina """
+
+h2("Function - Without Return")
+
+def soma(a, b):
+    print(a + b)
+
+soma(10, 30)
+
+""" O que acontece?
+
+Mostra o resultado na tela
+
+Mas o valor não pode ser reutilizado """
+
+resultado_soma = soma(20, 40)
+print(resultado_soma) # none
+
+""" Isso NÃO funciona
+
+Porque:
+A função não devolveu nada
+resultado_soma vira None """
+
+h2("Function - With Return")
+
+def subtracao(a, b):
+    return a - b
+
+resultado_subtracao = subtracao(100, 80)
+print(resultado_subtracao)
+
+""" Agora sim:
+
+A função devolve o valor correto
+
+Você pode guardar, usar, comparar, subtrair, etc """
+
+
+""" Diferença entre print e return (muito importante)
+
+
+| `print()`                | `return`             |
+| ------------------------ | -------------------- |
+| Mostra algo na tela      | Devolve um valor     |
+| Serve para visualizar    | Serve para lógica    |
+| Não pode ser reutilizado | Pode ser reutilizado |
+
+print nao substitui return """ 
+
+h2("Keep in mind: return ends the function")
+
+""" The return statement ends the function execution.
+
+When Python reaches return, the function stops running.
+
+return immediately exits the function. """
+
+# Tudo que vem depois do return não executa 
+
+def test():
+    print("Antes")
+    return 
+    print("Depois")
+
+test() # Antes
+
+h2("return types example")
+
+# return pode devolver qualquer coisa
+
+# Number 
+
+def dobro(n):
+    return n * 2 
+
+# Text 
+
+def saudacao():
+    return "Olá!"
+
+# Boolean 
+
+def eh_maio(n):
+    return n > 10
+
+# List 
+
+def criar_lista():
+    return[1, 2, 3]
+
+# Lógica (if / else)
+
+def verificar_idade(idade):
+    if idade >= 18:
+        return "Maior de idade"
+    else:
+        return "Menor de idade"
+    
+# Função para ter vários return 
+
+def classification(numero):
+    if numero > 0:
+        return "Positivo"
+    if numero < 0:
+        return "Negativo"
+    return "Zero"
+
+# Python executa o primeiro return que encontrar 
+
+""" Quando usar return?
+
+Use return quando você:
+
+Precisa guardar um valor
+
+Vai usar o resultado depois
+
+Quer testar a função
+
+Está criando lógica reutilizável
+
+Use print quando:
+
+Só quer mostrar algo para o usuário
+"""
+
+# Regra mental simples 
+
+""" Funções retornam valores
+Prints mostram valores """
+
+h2("Exercise 1")
+
+# Retornando um número
+
+# Crie uma função chamada retorna_numero que retorne o número 10.
+
+""" Dica:
+
+Use return 10
+
+Depois, use print() para mostrar o valor retornado """
+
+def retorna_numero():
+    return 10 
+
+print(retorna_numero) # <function retorna_numero at 0x000002D ...>
+
+print(retorna_numero()) # 10 
+
+# Forma alternativa (também correta)
+
+def retorna_numero():
+    return 10
+
+resultado = retorna_numero()
+print(resultado)
+
+h2("Exercise 2")
+
+# Retornando uma string 
+
+# Crie uma função chamada mensagem que retorne a string "Hello, Python!"
+
+""" Dica:
+
+return "Hello, Python!" """
+
+def mensagem():
+    return "Hello, Python!"
+
+print(mensagem())
+
+h2("Exercise 3")
+
+# Soma simples 
+
+""" Crie uma função soma que:
+
+Receba dois números
+
+Retorne a soma deles
+
+Dica:
+
+Não use print dentro da função
+
+Use return """
+
+def soma(d=30, e=50):
+    return d + e 
+
+print(soma()) # 80
+print(soma(10, 20)) # 30
+
+h2("Exercise 4")
+
+# Multiplicação
+
+""" Crie uma função multiplica que:
+
+Receba dois números
+
+Retorne o resultado da multiplicação """
+
+def multiplica(a=3, b=5):
+    return a * b
+
+print(multiplica())
+print(multiplica(4, 5))
+
+# segunda opção 
+
+def multiplica(a, b):
+    return a * b
+
+print(multiplica(1, 70))
+
+h2("Exercise 5")
+
+# Retornando um valor fixo 
+
+# Crie uma função status que sempre retorne a palavra "OK"
+
+def status():
+    return "OK"
+
+print(status())
+
+h2("Exercise 6")
+
+# Retornando o mesmo valo recebido 
+
+""" Crie uma função eco que:
+
+Receba um valor
+
+Retorne exatamente esse valor
+
+Dica:
+Isso ajuda a entender que return devolve dados """
+
+# Palavra
+
+def eco():
+    return input(f"Digite uma palavra: ")
+
+palavra = eco()
+
+print(palavra)
+print(f"A palavra digitada foi: {palavra}")
+
+# Número
+
+def eco_number():
+    return int(input("Digite um numero de 1 a 10: "))
+
+numero = eco_number()
+
+print(numero)
+print(f"O número digitado foi: {numero}")
+
+
+h2("Exercise 7")
+
+# Retornando um cálculo simples 
+
+""" Crie uma função quadrado que:
+
+Receba um número
+
+Retorne esse número elevado ao quadrado
+
+Dica:
+Use ** 2 
+"""
+
+def quadrado(a):
+    return a ** 2
+
+numero = int(input("Type any number from 1 to 10: "))
+
+print(quadrado(numero))
+
+h2("Exercise 8")
+
+# Retornando uma frase montada
+
+""" Crie uma função saudacao que:
+
+Receba um nome
+
+Retorne a frase:
+"Hello, <nome>!"
+
+Dica:
+Use f-string dentro do return """
+
+""" def saudacao():
+    nome = input("Digite o seu nome: ")
+    return nome 
+
+print(f"Olá, {saudacao(nome)}") """
+
+
+
+
+
